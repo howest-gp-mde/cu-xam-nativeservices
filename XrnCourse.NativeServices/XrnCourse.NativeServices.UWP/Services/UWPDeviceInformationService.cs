e1f81c550994366c072dd22571ea8d906ff08ae7
sigged <sderdeyn@gmail.com>
2019-10-28 19:51:05 +0100

    Add platform specific services to access device hardware information

---
+++ XrnCourse.NativeServices/XrnCourse.NativeServices.UWP/Services/UWPDeviceInformationService.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Windows.Devices.Haptics;
+using Windows.Devices.Sensors;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+using Xamarin.Forms;
+using XrnCourse.NativeServices.Domain;
+using XrnCourse.NativeServices.Domain.Services;
+
+[assembly: Dependency(typeof(XrnCourse.NativeServices.UWP.Services.UWPDeviceInformationService))]
+
+namespace XrnCourse.NativeServices.UWP.Services
+{
+    internal class UWPDeviceInformationService : IDeviceInformationService
+    {
+        EasClientDeviceInformation easDevice;
+        private Accelerometer accelerometer;
+        private Gyrometer gyrometer;
+        private Magnetometer magnetometer;
+        private VibrationDevice vibrationDevice;
+
+        public UWPDeviceInformationService()
+        {
+            easDevice = new EasClientDeviceInformation();
+            accelerometer = Accelerometer.GetDefault();
+            gyrometer = Gyrometer.GetDefault();
+            magnetometer = Magnetometer.GetDefault();
+            try
+            {
+                vibrationDevice = VibrationDevice.GetDefaultAsync().GetResults();
+            }
+            catch { }
+
+        }
+
+        public DeviceInformation GetDeviceInfo()
+        {
+            var info = new DeviceInformation
+            {
+                Manufacturer = easDevice.SystemManufacturer,
+                Model = easDevice.SystemManufacturer,
+                ReleaseVersion = easDevice.OperatingSystem,
+                CanVibrate = false,
+                Sensors = new List<string>()
+            };
+
+            if (vibrationDevice != null)
+                info.CanVibrate = true;
+            if (accelerometer != null)
+                info.Sensors.Add("Accelerometer ");
+            if (gyrometer != null)
+                info.Sensors.Add("Gyroscope ");
+            if (magnetometer != null)
+                info.Sensors.Add("Magnetometer");
+
+            return info;
+        }
+    }
+}
+
---
+++ XrnCourse.NativeServices/XrnCourse.NativeServices.iOS/Services/IOSDeviceInformationService.cs
@@ -0,0 +1,47 @@
+﻿using CoreLocation;
+using CoreMotion;
+using System.Collections.Generic;
+using UIKit;
+using Xamarin.Forms;
+using XrnCourse.NativeServices.Domain;
+using XrnCourse.NativeServices.Domain.Services;
+
+[assembly: Dependency(typeof(XrnCourse.NativeServices.iOS.Services.IOSDeviceInformationService))]
+
+namespace XrnCourse.NativeServices.iOS.Services
+{
+    internal class IOSDeviceInformationService : IDeviceInformationService
+    {
+        private UIDevice device;
+        private CMMotionManager motionManager;
+        private CLLocationManager locationManager;
+
+        public IOSDeviceInformationService()
+        {
+            device = new UIDevice();
+            motionManager = new CMMotionManager();
+            locationManager = new CLLocationManager();
+        }
+
+        public DeviceInformation GetDeviceInfo()
+        {
+            var info = new DeviceInformation
+            {
+                Manufacturer = "Apple",
+                Model = device.Model,
+                ReleaseVersion = device.SystemVersion,
+                CanVibrate = true,
+                Sensors = new List<string>()
+            };
+
+            if (motionManager.AccelerometerAvailable)
+                info.Sensors.Add("Accelerometer");
+            if (motionManager.GyroAvailable)
+                info.Sensors.Add("Gyroscope");
+            if (motionManager.MagnetometerAvailable)
+                info.Sensors.Add("Magnetometer");
+
+            return info;
+        }
+    }
+}
